fd8043ba492e32f3903c3fba193c64b95919f4fa
MarvJungs <Marvin10072000@live.de>
2024-01-10 00:12:56 +0100

    about page

--- about/index.php
+++ about/index.php
@@ -24,23 +24,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/_includes/includes.php';
 	<div class="container">
 		<?php include($_SERVER['DOCUMENT_ROOT'] . '/_includes/header.php'); ?>
 		<div align="center">
-			<h1>About</h1>
-			This webpage aims to have all Versions and all ROM Hacks of Super Mario 64 available. This webproject as a whole is maintained by <a href="https://twitch.tv/tomatobird8" target="_blank">Tomatobird8</a> and <a href="https://twitch.tv/marvjungs" target="_blank">MarvJungs</a> If you like what you see and would like to see possible improvements and further maintance of this website, feel free to put out a <a href="https://ko-fi.com/marvjungs" target="_blank">donation</a><br />
-			Technical Details: The Website is hosted by a domain provider located in Germany and the content is generated using PHP with HTMP DOM Manipulation with JavaScript and Data Storage in CSV- and JSON-Files. <br /><br />
-
-			<h2>Privacy Notice</h2>
-		</div>
-		<div align="left">
-			Upon visiting this webpage, we will collect the following information stored in a log-file but for no more than 60 days:
-			<ul>
-				<li>IP Address</li>
-				<li>Timestamp</li>
-				<li>HTTP Request</li>
-				<li>HTTP Response Code</li>
-				<li>Accessed Resource</li>
-				<li>Browser and Operating System</li>
-			</ul>
-			This data may be used to do Data Analysis in the near future but as of the date writing this (06th May 2023), no data processing is done. This section will be updated once needed.
+			<?php readfile($_SERVER['DOCUMENT_ROOT'] . '/_assets/_pages/about.html');?>
 			<?php include($_SERVER['DOCUMENT_ROOT'] . '/_includes/footer.php'); ?>
 		</div>
 	</div>
