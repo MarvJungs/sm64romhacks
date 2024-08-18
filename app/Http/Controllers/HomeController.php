606cd8d6828cf157413364b9b80627a02e7c085a
MarvJungs <Marvin10072000@live.de>
2024-08-19 01:00:30 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use App\Models\Version;
 use App\Models\News;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
-    public string $latest_runs_url = 'https://www.speedrun.com/api/v2/GetLatestLeaderboard?_r=eyJzZXJpZXNJZCI6IjA0OTlvNjR2IiwidmFyeSI6MTcxNjg2ODE4OX0';
     public function index()
     {
         $news = News::orderByDesc('created_at')->take(3)->get();
