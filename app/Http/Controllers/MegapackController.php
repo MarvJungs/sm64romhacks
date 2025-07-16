a1ce793aa062e3761db35f7bf8bd997e1005d9b1
Marvin Jung <Marvin10072000@live.de>
2025-07-16 16:24:59 +0200

    updated megapack logic

--- app/Http/Controllers/MegapackController.php
+++ app/Http/Controllers/MegapackController.php
@@ -47,13 +47,32 @@ class MegapackController extends Controller
 
     public function download(Request $request)
     {
-        $megapack_normal = 'Grand Rom Hack Megapack 2024 (Summer Edition)';
-        $megapack_kaizo = 'Grand SM64 Kaizo Megapack 2024 (Summer Edition)';
+        $megapack_normal = 'Grand ROM Hack Megapack 2025 (Summer Edition)';
+        $megapack_kaizo = 'Grand SM64 Kaizo Megapack 2025 (Summer Edition)';
 
         $type = $request->query('type');
 
         if ($type == 'normal' || empty($type)) {
-            return Storage::download('megapack/' . $megapack_normal . '.zip');
+            $filePath = storage_path('app/public/megapack/' . $megapack_normal . '.zip');
+            $headers = [
+                'Content-Type' => mime_content_type($filePath),
+                'Content-Disposition' => 'attachment; filename="' . basename($filePath) . '"',
+            ];
+            return response()->stream(
+                function () use ($filePath) {
+                    $stream = fopen($filePath, 'rb');
+                    if ($stream) {
+                        while (!feof($stream)) {
+                            echo fread($stream, 1024 * 8);
+                            flush();
+                            ob_flush();
+                        }
+                        fclose($stream);
+                    }
+                },
+                200,
+                $headers
+            );
         } else {
             return Storage::download('megapack/' . $megapack_kaizo . '.zip');
         }
