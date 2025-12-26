e66c49289bf794784b17fc933e39aa3ac34c23ba
Marvin Jung <Marvin10072000@live.de>
2025-12-26 11:38:24 +0100

    update

--- resources/js/tooltips.js
+++ resources/js/tooltips.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-});
+import { Tooltip } from "bootstrap";
+
+export default class Tooltips {
+    main() {
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
+    }
+}
