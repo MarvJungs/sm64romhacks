214c37c8fcfc9b55fb8b818914545ea24147f5c3
Marvin Jung <Marvin10072000@live.de>
2025-12-31 14:34:30 +0100

    feat: added tooltip to indicate time is to be given in utc

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -29,7 +29,9 @@
             @enderror
         </div>
         <div class="col-12">
-            <label for="start_utc" class="form-label">Starttime</label>
+            <label for="start_utc" class="form-label">Starttime</label><button type="button" class="btn p-1 mb-1"
+                data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Timezone: UTC"><x-bi-info-circle-fill
+                    class="text-primary" /></button>
             <input type="datetime-local" class="form-control @error('start_utc') is-invalid @enderror" name="start_utc"
                 value="{{ old('start_utc') ?? $event->start_utc }}">
             @error('start_utc')
@@ -37,9 +39,11 @@
             @enderror
         </div>
         <div class="col-12">
-            <label for="end_utc" class="form-label">Endtime</label>
+            <label for="end_utc" class="form-label">Endtime</label><button type="button" class="btn p-1 mb-1"
+                data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Timezone: UTC"><x-bi-info-circle-fill
+                    class="text-primary" /></button>
             <input type="datetime-local" class="form-control @error('end_utc') is-invalid @enderror" name="end_utc"
-                value="{{ old('end_utc') ?? $event->end_utc}}">
+                value="{{ old('end_utc') ?? $event->end_utc }}">
             @error('end_utc')
                 <p class="text-danger">{{ $message }}</p>
             @enderror
@@ -47,7 +51,8 @@
         <div class="col-12">
             <div class="form-check">
                 <input class="form-check-input" type="hidden" name="external" value="0">
-                <input class="form-check-input" type="checkbox" name="external" value="1" {{ $event->external ? 'checked' : '' }}>
+                <input class="form-check-input" type="checkbox" name="external" value="1"
+                    {{ $event->external ? 'checked' : '' }}>
                 <label class="form-check-label" for="external">
                     External
                 </label>
@@ -56,12 +61,13 @@
         <div class="col-12">
             <label for="" class="form-label">Link</label>
             <input type="url" class="form-control @error('external_url') is-invalid @enderror" name="external_url"
-                value="{{ old('external_url') ?? $event->external_url}}">
+                value="{{ old('external_url') ?? $event->external_url }}">
             @error('external_url')
                 <p class="text-danger">{{ $message }}</p>
             @enderror
         </div>
-        <input type="hidden" id="description" name="description" value="{{ old('description') ?? json_encode($event->description) }}" />
+        <input type="hidden" id="description" name="description"
+            value="{{ old('description') ?? json_encode($event->description) }}" />
         <div class="col-12">
             <button id="eventSubmitButton" type="submit" class="btn btn-primary">Create Event</button>
         </div>
