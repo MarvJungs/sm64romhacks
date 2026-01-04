738c0d78ec3f7894775d504a6869c875741d0470
Marvin Jung <Marvin10072000@live.de>
2026-01-04 17:05:05 +0100

    Update config file

--- config/services.php
+++ config/services.php
@@ -47,5 +47,11 @@ return [
         'client_id' => env('TWITCH_CLIENT_ID'),
         'client_secret' => env('TWITCH_CLIENT_SECRET'),
         'redirect' => env('TWITCH_REDIRECT_URI')
+    ],
+
+    'google' => [
+        'api_key' => env('GOOGLE_API_KEY'),
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET')
     ]
 ];
