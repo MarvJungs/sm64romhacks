03aa9792af4747617daf2711d946a138ae3e895e
Marvin Jung <Marvin10072000@live.de>
2026-01-11 13:55:14 +0100

    feat: request classes for event runs validation

---
+++ app/Http/Requests/UpdateRomhackeventrunRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Romhackevent;
+use App\Rules\YoutubeVideoUrl;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRomhackeventrunRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $event = Romhackevent::find($this->route('event'))->first();
+        return $event && $this->user()->can('update', $event);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'romhack' => 'string|required',
+            'category' => 'string|required',
+            'type' => 'string|required',
+            'videolink' => 'array|list|required|min:1',
+            'videolink.*' => ['url', 'active_url', 'distinct', new YoutubeVideoUrl]
+        ];
+    }
+
+}
